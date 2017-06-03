9b1582132978781b55c632146390243931119751
kid93 <uikid93@gmail.com>
2017-06-03 23:19:03 +0100

    Added Comments to Show Output

--- StackOverFlowPost/Program.cs
+++ StackOverFlowPost/Program.cs
@@ -20,6 +20,13 @@ namespace StackOverFlowPost
             Console.WriteLine(String.Format("Post Creation Date: {0:MMM dd} \'{0:yy} at {0:HH:mm}", post.CreationDate));
             Console.WriteLine("Post Votes: {0}", post.CurrentVotes);
 
+
+            // OUTPUT
+            // Post Title: My title
+            // Post Description: My description
+            // Post Creation Date: Jun 03 '17 at 23:17
+            // Post Votes: 2
+          
         }
     }
 }
